fa7effea67450af6d73772732e84a2d36c921af5
Ziya SARIKAYA <sarikayaziya@gmail.com>
2013-03-04 15:04:53 +0200

    Extended IRepository interface for simple dynamic query support

---
+++ RepositoryT.EntityFramework/IEFRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using RepositoryT.Infrastructure;
+
+namespace RepositoryT.EntityFramework
+{
+    public interface IEFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        List<TDynamicEntity> GetDynamic<TTable, TDynamicEntity>(Expression<Func<TTable, object>> selector, Func<object, TDynamicEntity> maker) where TTable : class;
+        List<TDynamicEntity> GetDynamic<TTable, TDynamicEntity>(Func<TTable, object> selector, Func<object, TDynamicEntity> maker) where TTable : class;
+    }
+}
